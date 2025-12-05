d39c776c1111de19b83ca732a250804e54697050
¨gonmpr¨ <gmporrorosas@gmail.com>
2025-12-05 18:28:47 -0300

    finally, a piece i can move and falls!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys, tty, termios, time
 from tools import set_terminal, read_char, restart_terminal, draw, clear_terminal
 from board import board_state, make_empty_board
 from constants import SYMBOL
+from piece import stick
 
 def main():
     last_time = time.time()
@@ -9,12 +10,6 @@ def main():
 
     set_terminal() # set the terminal to game mode
     make_empty_board() # creates an empty board
-
-    board_state [0][2] = SYMBOL
-    board_state [1][2] = SYMBOL
-
-
-
 
 
 
@@ -26,10 +21,16 @@ def main():
         if char == 'q':
             break
 
-
+        if char in ['a', 'd']:
+            stick.move_sideways(char)
+            stick.draw_piece()
+            draw()
 
 
         if current_time - last_time >= frame_delay: #if the time passed, it will render 
+            stick.move_sideways(char)
+            stick.move_down()
+            stick.draw_piece()
             draw()
 
 
--- piece.py
+++ piece.py
@@ -1,29 +1,48 @@
 import sys, tty, termios, time
-from tools import set_terminal, read_char, restart_terminal, draw, clear_terminal
-
+from tools import set_terminal, restart_terminal, draw, clear_terminal
+from board import board_state # board[row][col]
+from constants import SYMBOL, BOARD_WIDTH, BOARD_HEIGHT
 
 class Piece:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.parts = []
-        self.canMove = True
+    def __init__(self, form):
+        self.previous_position = form #[(a,b)(a,b)]
+        self.next_position = [] #[(a,b)(a,b)]
 
 
-    def move(self):
-        char = read_char()
+    def move_sideways(self, char):
+        self.next_position = []
 
         if char == 'a':
-            #move all left
-        if char == 'd':
-            #move all right 
+            for pos in self.previous_position:
+                if pos[1] - 1 >= 0:
+                    self.next_position.append((pos[0], pos[1]-1))
+                else:
+                    self.next_position.append((pos[0], pos[1]))
 
-    def rotate(self):
-        char = read_char()
+        elif char == 'd':
+            for pos in self.previous_position:
+                if pos[1] + 1 < BOARD_WIDTH:
+                    self.next_position.append((pos[0], pos[1]+1))
+                else:
+                    self.next_position.append((pos[0], pos[1]))
 
-        if char == 'r':
-            #rotates clockwise
+    def move_down(self):
+        self.next_position = []
 
-    def collides_with(self, other)
-            #if bottom part collides with a block, it stop
-            #if it touch the borders, can no move to that side
+        for pos in self.previous_position:
+            if pos[0] + 1 < BOARD_HEIGHT:
+                self.next_position.append((pos[0]+1, pos[1]))
+
+
+    def draw_piece(self):
+        global board_state
+
+        for pos in self.previous_position:
+            board_state[pos[0]][pos[1]] = ' '
+        for pos in self.next_position:
+            board_state[pos[0]][pos[1]] = SYMBOL
+
+        self.previous_position = self.next_position.copy()
+
+
+stick = Piece([(1,3),(2,3),(3,3)])
